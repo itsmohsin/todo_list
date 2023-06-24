ba92e4c189bcb7b958a8a2dfa54ab8fec0b97b96
Mohsin Khan <mohsinsmannat@gmail.com>
2023-06-24 18:53:07 +0530

    addded todo_tile

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/components/dialog_box.dart';
+import 'package:todo_list/components/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // list of todo task
+  List toDoList = [
+    ["Learn Flutter", false],
+    ["Do Exercise", false],
+  ];
+
+  // checkbox was tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  // create a new task
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox();
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('TO DO'),
+        elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
+      ),
+    );
+  }
+}
